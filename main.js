45440d8c2115b6204769e0b12c7946cadb13f580
JohnMnyika <jmnyika514@gmail.com>
2022-04-17 23:58:51 +0300

    Disable past dates from the date-picker

--- main.js
+++ main.js
@@ -16,15 +16,37 @@ if (Array.isArray(savedTodos)) {
 window.addEventListener('load', () => {
     toggleDeleteBtn(todos);
 });
+
+
+
 document.getElementById('addTasks').addEventListener('click', function() {
     addTodo(todos);
     // get the title input
     const todoTitleInput = document.getElementById('todo-title');
     // get the due date input
     const dueDateInput = document.getElementById('date-picker');
+
+    
+    
+
+    //Disable past days
+    let date = new Date();
+    let tdate = date.getDate();
+    let month = date.getMonth() + 1;
+    if (tdate < 10) {
+        tdate = "0" + tdate;
+    }
+    if (month < 10) {
+        month = "0" + month;
+    }
+    let year = date.getUTCFullYear();
+    let minDate = year + "-" + month + "-" + tdate;
+    document.getElementById("date-picker").setAttribute('min', minDate);
+
     // clear the inputs
     todoTitleInput.value = "";
     dueDateInput.value = "";
+
 });
 
 render(todos);
